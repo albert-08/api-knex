5209557ec87128020f9af63e10303dcfe56cdff0
Alberto Flores <alberto.floreso94@gmail.com>
2021-03-03 19:39:48 -0600

    edit authenticate.js and userController.js

--- utils/authenticate.js
+++ utils/authenticate.js
@@ -4,14 +4,15 @@ const bcrypt = require('bcrypt')
 module.exports = ({email,password}) => {
     return new Promise((resolve,reject) => {
         UserModel.find({email})
-                    .then((result) => {
-                        const [user] = result
-                        bcrypt.compare(password,user.password,function(err,same){
-                            same ? resolve({same, user})
-                                 : reject(new Error('El password es incorrecto'))
-                        })
-                    }).catch((error) => {
-                        reject(error)
+                .then((result) => {
+                    const [user] = result
+                    if(!user) reject (new Error('El usuario no existe'))
+                    bcrypt.compare(password,user.password,function(err,same){
+                        same ? resolve({same, user})
+                             : reject(new Error('El password es incorrecto'))
                     })
+                }).catch((error) => {
+                    reject(error)
+                })
     })
 }
